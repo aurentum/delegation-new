dc59ac906cf01aae54f7f15a1194c7a484bc3c1f
Clauzi <taranuclaudiu@gmail.com>
2022-06-13 22:17:26 +0300

    first commit

---
+++ src/assets/LogoAurentum.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from '../components/Layout/Navbar/styles.module.scss';
+
+const LogoAurentum = () => (
+  <img src='/logo-aurentum.svg' alt='aurentum-logo' className={styles.logo} />
+);
+
+export default LogoAurentum;
